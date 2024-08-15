a88b422d47e0862a21bdf14de011df05b85ee060
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-08-15 14:19:16 +0530

    Added 1_d

--- Assignment1/A1_1_d.cpp
+++ Assignment1/A1_1_d.cpp
@@ -9,14 +9,14 @@ long long fibo(int n, vector<long long>& fibonum){
         fibonum[n-1] = n-1;
         return fibonum[n-1];
     }
-    fibonum[n-1] = fibo(n-1, fibonum) + fibo(n-2, fibonum);
+    fibonum[n-1] = fibonum[n-2] + fibonum[n-3];
     return fibonum[n-1];
 }
 
 int main(){
     int n = 50;
     vector<long long> fibonum(n, -1);
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n; i++){ //this loop works to calculate the new values too
         printf("fibo(%d) = %lld \n", i+1, fibo(i+1, fibonum));
     }
     return 0;
