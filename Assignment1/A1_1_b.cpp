3d03782672c640e73ff6a7443d62713550ee0b95
ThunderMJ2003 <manav.jain@iitgn.ac.in>
2024-08-16 21:43:51 +0530

    Added times to 1

--- Assignment1/A1_1_b.cpp
+++ Assignment1/A1_1_b.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <windows.h>
 using namespace std;
 
 long long fibo(int n){
@@ -16,9 +17,27 @@ long long fibo(int n){
 }
 
 int main(){
-    int n = 50;
-    for (int i=0; i<n; i++){
-        printf("fibo(%d) = %lld \n", i+1, fibo(i+1));
+    LARGE_INTEGER freq;
+    LARGE_INTEGER start, end;
+    QueryPerformanceFrequency(&freq);
+    vector<double> times;
+    double total_time = 0;
+    for (int x=0; x<10; x++){
+        QueryPerformanceCounter(&start);
+        int n = 50;
+        for (int i=0; i<n; i++){
+            printf("fibo(%d) = %lld \n", i+1, fibo(i+1));
+        }
+        QueryPerformanceCounter(&end);
+        double time_taken = static_cast<double>(end.QuadPart - start.QuadPart) / freq.QuadPart;
+        times.push_back(time_taken);
+        total_time += time_taken;
     }
+    printf("All times: \n");
+    for (int x=0; x<10; x++){
+        printf("%f ", times[x]);
+    }
+    printf("\n");
+    printf("Average time = %f \n", total_time/10.0);
     return 0;
 }
--- Assignment1/Trial.cpp
+++ Assignment1/Trial.cpp
@@ -1,15 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <windows.h>
 
-void foo(int n, vector<int>& v){
-    v[n] = 0;
+void someFunction() {
+    // Example function that just sleeps for 2 seconds
+    Sleep(2); // Sleep for 2000 milliseconds (2 seconds)
 }
 
-int main(){
-    vector<int> t(10, -1);
-    foo(4, t);
-    for (int i=0; i<10; i++){
-        printf("%d ", t[i]);
-    }
+int main() {
+    LARGE_INTEGER frequency;        // Stores the frequency of the performance counter
+    LARGE_INTEGER start, end;       // Stores the starting and ending counts
+
+    // Get the frequency of the performance counter
+    QueryPerformanceFrequency(&frequency);
+
+    // Get the starting count
+    QueryPerformanceCounter(&start);
+
+    // Call the function whose execution time you want to measure
+    someFunction();
+
+    // Get the ending count
+    QueryPerformanceCounter(&end);
+
+    // Calculate the time taken in seconds
+    double time_taken = static_cast<double>(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+
+    std::cout << "Time taken by someFunction: " << time_taken << " seconds" << std::endl;
+
     return 0;
 }
